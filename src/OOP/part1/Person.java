b1c127e4ef7bcf3b081a1da5cce107a0e6608ab1
mxie <836728902@qq.com>
2020-03-26 21:27:20 +0200

    constructor

--- src/OOP/part1/Person.java
+++ src/OOP/part1/Person.java
@@ -1,7 +1,55 @@
 package OOP.part1;
 
 public class Person {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        int maxAge = 100;
+        if (age >= 0 && age <= maxAge) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+
+    }
+
+    public boolean isTeen() {
+        return this.age > 12 && this.age < 20;
+    }
+
+    public String getFullName() {
+        String fullName = "";
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return fullName;
+        }
+        if (lastName.isEmpty()) {
+            return firstName;
+        }
+        if (firstName.isEmpty()) {
+            return lastName;
+        }
+        return firstName + " " + lastName;
     }
 }
