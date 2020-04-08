a1cca09adad5ccafd6c12f861ce183ca3609cf1a
mxie <836728902@qq.com>
2020-04-08 23:03:14 +0300

    Inheritance code challenge

---
+++ src/OOP/part1/inheritance/Cuboid.java
@@ -0,0 +1,21 @@
+package OOP.part1.inheritance;
+
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0) {
+            height = 0;
+        }
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return height * getArea();
+    }
+}
