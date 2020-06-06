3faa22290eb761d6a9cf43fccbc30b46cf662ae6
mxie <836728902@qq.com>
2020-06-06 23:04:59 +0300

    Abstract class challenge.

--- src/AbstractClass/MyList.java
+++ src/AbstractClass/MyList.java
@@ -2,17 +2,55 @@ package AbstractClass;
 
 public class MyList implements NodeList {
     private ListItem root = null;
-    
+
     public MyList(ListItem root) {
         this.root = root;
     }
+
     @Override
     public ListItem getRoot() {
-        return null;
+        return root;
     }
 
     @Override
     public boolean addItem(ListItem item) {
+        if (this.root == null) {
+            this.root = item;
+            return true;
+        }
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(item);
+            if (comparison < 0) {
+                //new one is greater, move right
+                if (currentItem.nextItem != null) {
+                    currentItem = currentItem.nextItem;
+                } else {
+                    //there is no nextItem, reach at the end of the list
+                    currentItem.setNextItem(item);
+                    item.setPreviousItem(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                //new item is less, insert before
+                if (currentItem.getPreviousItem() != null) {
+                    currentItem.getPreviousItem().setNextItem(item);
+                    item.setPreviousItem(currentItem.getPreviousItem());
+                    item.setNextItem(currentItem);
+                    currentItem.setPreviousItem(item);
+                } else {
+                    //the root
+                    item.setNextItem(root);
+                    root.setPreviousItem(item);
+                    root = item;
+                }
+                return true;
+            } else {
+                //equal
+                System.out.println(item.getValue() + " is already pressed.");
+                return false;
+            }
+        }
         return false;
     }
 
