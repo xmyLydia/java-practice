3041b23c4d3f3bd461335441094908b5c0b67289
mxie <836728902@qq.com>
2020-06-20 21:55:31 +0300

    Package challenge

---
+++ src/PackageChallenge/Main.java
@@ -0,0 +1,12 @@
+package PackageChallenge;
+
+/**
+ * @author mingyux
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Series series = new Series();
+        System.out.println(series.fibonacci(10));
+    }
+}
---
+++ src/PackageChallenge/Series.java
@@ -0,0 +1,31 @@
+package PackageChallenge;
+
+/**
+ * @author mingyux
+ */
+public class Series {
+    public int nSum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + nSum(n - 1);
+    }
+
+    public int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
