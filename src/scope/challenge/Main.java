e68736efa0cf249a31eb0ee0c675a8082451ceb7
mxie <836728902@qq.com>
2020-06-23 22:30:17 +0300

    scope challenge

---
+++ src/scope/challenge/Main.java
@@ -0,0 +1,15 @@
+package scope.challenge;
+
+import java.util.Scanner;
+
+/**
+ * @author mingyux
+ */
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = scanner.nextInt();
+        System.out.println();
+    }
+}
