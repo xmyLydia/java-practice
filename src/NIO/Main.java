919e9e1dac93df86f2c1049e9f7ab473fa39f5a7
mxie <836728902@qq.com>
2020-08-23 23:50:08 +0300

    java NIO

--- src/NIO/Main.java
+++ src/NIO/Main.java
@@ -1,65 +1,81 @@
 package NIO;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 public class Main {
     private static Locations locations = new Locations();
 
     public static void main(String[] args) {
-        // Change the program to allow players to type full words, or phrases, then move to the
-        // correct location based upon their input.
-        // The player should be able to type commands such as "Go West", "run South", or just "East"
-        // and the program will move to the appropriate location if there is one.  As at present, an
-        // attempt to move in an invalid direction should print a message and remain in the same place.
-        //
-        // Single letter commands (N, W, S, E, Q) should still be available.
+        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()) {
 
-	    Scanner scanner = new Scanner(System.in);
-
-        Map<String, String> vocabulary = new HashMap<String, String>();
-        vocabulary.put("QUIT", "Q");
-        vocabulary.put("NORTH", "N");
-        vocabulary.put("SOUTH", "S");
-        vocabulary.put("WEST", "W");
-        vocabulary.put("EAST", "E");
-
-
-        int loc = 64;
-//        int loc = 1;
-        while(true) {
-            System.out.println(locations.get(loc).getDescription());
-
-            if(loc == 0) {
-                break;
-            }
-
-            Map<String, Integer> exits = locations.get(loc).getExits();
-            System.out.print("Available exits are ");
-            for(String exit: exits.keySet()) {
-                System.out.print(exit + ", ");
-            }
-            System.out.println();
-
-            String direction = scanner.nextLine().toUpperCase();
-            if(direction.length() > 1) {
-                String[] words = direction.split(" ");
-                for(String word: words) {
-                    if(vocabulary.containsKey(word)) {
-                        direction = vocabulary.get(word);
-                        break;
-                    }
-                }
-            }
-
-            if(exits.containsKey(direction)) {
-                loc = exits.get(direction);
-
-            } else {
-                System.out.println("You cannot go in that direction");
-            }
+            byte[] outputBytes = "Hello World!".getBytes();
+//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
+//            buffer.put(outputBytes);
+//            buffer.flip();
+//            int numBytes = binChannel.write(buffer);
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+//            intBuffer.putInt(245);
+//            intBuffer.flip();
+//            numBytes = binChannel.write(intBuffer);
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            intBuffer.flip();
+//            intBuffer.putInt(-98765);
+//            intBuffer.flip();
+//            numBytes = binChannel.write(intBuffer);
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            FileChannel channel = ra.getChannel();
+//            outputBytes[0] = 'a';
+//            outputBytes[1] = 'b';
+//            buffer.flip();
+//            long numBytesRead = channel.read(buffer);
+//            if(buffer.hasArray()) {
+//                System.out.println("byte buffer = " + new String(buffer.array()));
+//            }
+//
+//            // Absolute read
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            System.out.println(intBuffer.getInt(0));
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            System.out.println(intBuffer.getInt(0));
+//
+//
+//            // Relative read
+//            //            intBuffer.flip();
+//            //            numBytesRead = channel.read(intBuffer);
+//            //            intBuffer.flip();
+//            //            System.out.println(intBuffer.getInt());
+//            //            intBuffer.flip();
+//            //            numBytesRead = channel.read(intBuffer);
+//            //            intBuffer.flip();
+//            //            System.out.println(intBuffer.getInt());
+//
+//            channel.close();
+//            ra.close();
+//
+////            System.out.println("outputBytes = " + new String(outputBytes));
+//
+////            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+////            byte[] b = new byte[outputBytes.length];
+////            ra.read(b);
+////            System.out.println(new String(b));
+////
+////            long int1 = ra.readInt();
+////            long int2 = ra.readInt();
+////            System.out.println(int1);
+////            System.out.println(int2);
+//
+        } catch(IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
