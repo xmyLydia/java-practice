c9021623f4f6c7787b8045fd6f1ff81d30afefe5
mxie <836728902@qq.com>
2020-09-13 00:44:41 +0300

    Lambda expression.

--- src/lambdaChallenge/Main.java
+++ src/lambdaChallenge/Main.java
@@ -1,7 +1,10 @@
 package lambdaChallenge;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,6 +40,40 @@ public class Main {
         Supplier<String> iLoveJava = () -> "I love Java";
         String supplierResult = iLoveJava.get();
         System.out.println(supplierResult);
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+//        List<String> firstUpperCaseList = new ArrayList<>();
+//        topNames2015.forEach(name -> firstUpperCaseList.add(name.substring(0, 1).toUpperCase()
+//                        .concat(name.substring(1, name.length()))
+//                )
+//        );
+//        firstUpperCaseList.sort(String::compareTo);
+//        firstUpperCaseList.forEach(System.out::println);
+
+//        Long count = topNames2015
+//                .stream()
+//                .map(s -> s.substring(0, 1).toUpperCase().concat(s.substring(1, s.length())))
+//                .sorted(String::compareTo)
+//                .filter(s -> s.charAt(0) == 'A')
+//                .count();
+//        System.out.println("A:" + count);
+
+        List<String> updatedList = topNames2015
+                .stream()
+                .map(s -> s.substring(0, 1).toUpperCase().concat(s.substring(1, s.length())))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
     }
 
     public static String everySecondChar(Function<String, String> lambdaFunction, String input) {
