e96cd1f1f3a1fd42a03f95d0453ea520f00bdfd7
mxie <836728902@qq.com>
2020-10-07 21:43:52 +0300

    Debug && Junit

---
+++ src/UnitTest/ChallengeTest.java
@@ -0,0 +1,22 @@
+package UnitTest;
+
+import org.junit.Test;
+
+class ChallengeTest {
+
+    @Test
+    void everyNthChar() {
+    }
+
+    @Test
+    void removePairs() {
+    }
+
+    @Test
+    void converter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nullIfOddLength() {
+    }
+}
